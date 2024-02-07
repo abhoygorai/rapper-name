62f01affae34657943288dd0f458c164c2a133f7
Abhoy Gorai <abhoygorai04@gmail.com>
2024-02-08 02:27:18 +0530

    change for the version change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const data = ['21 Savage', 'Kanye West', 'Travis Scott', 'Kedrik Lamar', 'J Cole', 'Eminem', 'Future', 'Gunna', 'Don Toliver', 'Juice World', 'Kodak Black', 'Playboi Carti', 'Swae Lee', 'Young Thug', 'Drake', 'Lil Baby', 'Jay Z', '2Pac'];
+const data = ['21 Savage', 'Kanye West', 'Travis Scott', 'Kedrik Lamar', 'J Cole', 'Eminem', 'Future', 'Gunna', 'Don Toliver', 'Juice World', 'Kodak Black', 'Playboi Carti', 'Swae Lee', 'Young Thug', 'Drake', 'Lil Baby', 'Jay Z', '2Pac', 'Dababy'];
 
 function getName(){
     const l = data.length;
